2f4c3a588b90a6e4071346cb11f86b19f3b26046
Carlos Zamora <carlos14zamora@gmail.com>
2024-03-27 00:22:05 -0600

    summarize weekly pay by department with the help of the PARTITION BY clause

---
+++ SQL Practice Window Functions/summarize-weekly-pay-by-department.sql
@@ -0,0 +1,11 @@
+SELECT
+  EmployeeID,
+  Department,
+  Position,
+  WeeklyPay,
+  SUM(WeeklyPay) OVER(PARTITION BY Department) AS DeptTotal
+FROM
+  Employees
+ORDER BY
+  Department,
+  WeeklyPay;
