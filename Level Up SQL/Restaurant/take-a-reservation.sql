0efa19b5ca840ebd0842e2264b6e26ffbbd210c2
Carlos Zamora <carlos14zamora@gmail.com>
2024-04-07 21:17:18 -0600

    insert a new customer and create a reservation for them

---
+++ Level Up SQL/Restaurant/take-a-reservation.sql
@@ -0,0 +1,15 @@
+-- Create a reservation for a customer who may or may not
+-- already be listed in our Customers table.
+
+-- Use the following information:
+-- Sam McAdams (smac@kinetecoinc.com), for 5 people
+-- on August 12, 2022 at 6PM (18:00)
+INSERT INTO Customers (FirstName, LastName, Email)
+VALUES ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+
+INSERT INTO Reservations (CustomerID, Date, PartySize)
+VALUES ((
+  SELECT CustomerID
+  FROM Customers
+  WHERE Email = 'smac@kinetecoinc.com'
+), '2022-08-12 18:00:00', 5);
